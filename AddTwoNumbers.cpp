b56935c8d9f3439969c27a1e68db6720c1c4f1e3
woods-jiang <woods.jiang.1@gmail.com>
2015-04-10 17:05:51 +0800

    v2

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -54,3 +54,58 @@ public:
         return result;
     }
 };
+
+//-------------------------------------------------------
+// V2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *pTmp = new ListNode(0);
+        ListNode *tail = pTmp;
+        int carry = 0;
+        
+        while (l1 || l2) 
+        {
+            int x = carry;
+            
+            if (l1) 
+            {
+                x += l1->val;
+                l1 = l1->next;
+            }
+            
+            if (l2) 
+            {
+                x += l2->val;
+                l2 = l2->next;
+            }
+            
+            tail->next = new ListNode(x % 10);
+            tail = tail->next;
+            carry = x / 10;
+        }
+        
+        if (carry == 1) 
+        {
+            tail->next = new ListNode(carry);
+        }
+        
+        l1 = pTmp->next;
+        
+        if(pTmp!=NULL)
+        {
+            delete pTmp;
+            pTmp = NULL;
+        }
+        
+        return l1;
+    }
+};
