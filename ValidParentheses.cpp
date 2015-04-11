be43a37aaade2580ac9f12400e263b243e3e1d6e
woods-jiang <woods.jiang.1@gmail.com>
2015-04-12 07:37:58 +0800

    v1

---
+++ ValidParentheses.cpp
@@ -0,0 +1,46 @@
+﻿class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> stack_t;
+        
+        for (unsigned int i = 0; i < s.size(); i++)
+        {
+            switch (s[i])
+            {
+                case '(':
+                case '[':
+                case '{':
+                {
+                    stack_t.push(s[i]);
+                    break;
+                }
+                case ')':
+                {
+                    if (stack_t.empty() || stack_t.top() != '(')
+                        return false;
+                    stack_t.pop();
+                    break;
+                }
+                case ']':
+                {
+                    if (stack_t.empty() || stack_t.top() != '[')
+                        return false;
+                    stack_t.pop();
+                    break;
+                }
+                case '}':
+                {
+                    if (stack_t.empty() || stack_t.top() != '{')
+                        return false;
+                    stack_t.pop();
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+        
+        return stack_t.empty();
+    }
+};
