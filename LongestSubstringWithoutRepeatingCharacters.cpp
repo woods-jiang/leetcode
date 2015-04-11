f96c229b2a62e9eefb6b3c9c97b5949bfbe0bbe0
woods-jiang <woods.jiang.1@gmail.com>
2015-04-11 17:45:36 +0800

    v1

---
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,26 @@
+﻿class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        int pos[256];
+        
+        memset(pos,-1, sizeof(pos));
+        
+        int result = 0;
+        int last = -1; 
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            char c = s[i];
+            
+            if(pos[c] > last) //出现了重复的字符
+            {
+                last = pos[c];
+            }
+            
+            pos[c] = i;
+            result = max(result, i-last);
+        }
+        return result;
+    }
+};
