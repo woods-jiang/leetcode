c13917e3f56ebd0757e6cd9e789856d3c8a737fc
woods-jiang <woods.jiang.1@gmail.com>
2015-04-11 21:07:45 +0800

    v1

---
+++ BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int minPrice = INT_MAX, profit = 0;
+        
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+            else if (prices[i] - minPrice > profit)
+            {
+                profit = prices[i] - minPrice;
+            }
+        }
+        
+        return profit;
+    }
+};
