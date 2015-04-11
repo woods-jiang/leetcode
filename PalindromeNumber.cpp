fe5f1d94405f46684c3b60cce8fe574877a177cf
woods-jiang <woods.jiang.1@gmail.com>
2015-04-11 20:34:19 +0800

    v1

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,31 @@
+﻿class Solution {
+public:
+    bool isPalindrome(int x) {
+            
+        if(x < 0)
+            return false;
+            
+        int tmpX = x;
+        int count = 1;
+        
+        // x的位数等于count的位数
+        while(tmpX>=10)
+        {
+            count = count*10;
+            tmpX = tmpX/10;
+        }
+        
+        // 开始比较最后一位和第一位
+        while(count >= 10)
+        {
+            int high = x/count;
+            int low = x%10;
+            if(high!=low)
+                return false;
+            x = x%count;
+            x = x/10;
+            count = count/100;
+        }
+        return true;
+    }
+};
