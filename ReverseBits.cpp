fd44fb43bbb74c07f0ab1a6fd2176d3592996072
woods-jiang <woods.jiang.1@gmail.com>
2015-04-13 14:20:40 +0800

    v1

---
+++ ReverseBits.cpp
@@ -0,0 +1,21 @@
+﻿class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t one=1;
+        uint32_t result=0;
+        for(int i=0;i<32;i++)
+        {
+            
+            result = result << 1;
+            if(n & one)
+            {
+                result += 1;
+                
+            }
+            one=one<<1;
+        }
+        
+        return result;
+
+    }
+};
