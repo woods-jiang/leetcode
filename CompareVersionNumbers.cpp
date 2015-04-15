d28b81b187d1c7a1e9ecc9b7ab02c50068c2730b
Woods Jiang <woods.jiang.1@gmail.com>
2015-04-15 08:27:25 +0800

    v1

---
+++ CompareVersionNumbers.cpp
@@ -0,0 +1,40 @@
+﻿class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+      int index1 = 0, index2 = 0;
+    	while (index1!=version1.length() || index2!=version2.length())
+    	{
+    		int sum1 = 0, sum2 = 0;
+    		while (index1 != version1.length())
+    		{
+    			if (version1[index1] == '.')
+    			{
+    				index1++;
+    				break;
+    			}
+    			sum1 = sum1*10 + version1[index1] - '0';
+    			index1++;
+    		}
+    		while (index2 != version2.length())
+    		{
+    			if (version2[index2] == '.')
+    			{
+    				index2++;
+    				break;
+    			}
+    			sum2 = sum2*10 + version2[index2] - '0';
+    			index2++;
+    		}
+    		if (sum1 > sum2)
+    		{
+    			return 1;
+    		}
+    		else if (sum1 < sum2)
+    		{
+    			return -1;
+    		}
+    	}
+    
+    	return 0;
+    }
+};
