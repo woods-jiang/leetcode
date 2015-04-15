2e506ba8aba8038e217df709b0afd130be2a7777
Woods Jiang <woods.jiang.1@gmail.com>
2015-04-15 08:31:05 +0800

    v1

---
+++ ContainerWithMostWater.cpp
@@ -0,0 +1,22 @@
+﻿int maxArea(int height[], int n) {
+    int maxVolume = 0;
+    for(int i=0,j=n-1; i<n; )
+    {
+        int v=0;
+        if(height[i]<=height[j])
+        {
+            v=(j-i)*height[i];
+            i++;
+        }
+        else
+        {
+            v=(j-i)*height[j];
+            j--;
+        }
+        if(v>maxVolume)
+        {
+            maxVolume=v;
+        }
+    }
+    return maxVolume;
+}
