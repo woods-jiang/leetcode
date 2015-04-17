70cd60fd86fc61bf6c306828a02fb3e91501a4d4
Woods Jiang <woods.jiang.1@gmail.com>
2015-04-17 16:06:11 +0800

    v1

---
+++ sqrt_x.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    int mySqrt(int x) {
+        
+        if (x == 0)
+            return 0;
+            
+        double lastY = 0.0;
+        double y = 1.0;
+        
+        while (y != lastY)
+        {
+            lastY = y;
+            y = (y + x / y) / 2.0;
+        }
+        return (int)(y);
+    }
+};
