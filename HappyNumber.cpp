67efd7d57597cece3633789b6183cf33381671f3
Woods Jiang <woods.jiang.1@gmail.com>
2015-04-22 10:48:41 +0800

    v1

---
+++ HappyNumber.cpp
@@ -0,0 +1,25 @@
+﻿class Solution {
+public:
+    bool isHappy(int n) {
+        
+        int count = 10;     // 设置10次居然都能AC，不过还是设置大一点好些吧
+        
+        while(count--)
+        {
+            long long sum = 0;
+            
+            while(n!=0)
+            {
+                sum += pow(n%10,2);
+                n = n/10;
+            }
+            
+            n = sum;
+            
+            if(1 == sum)
+                return true;
+        }
+        
+        return false;
+    }
+};
