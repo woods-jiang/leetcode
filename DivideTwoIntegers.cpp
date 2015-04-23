04c3cc7eeef8e4440bdd4f751c23490967a8fb5e
woods-jiang <woods.jiang.1@gmail.com>
2015-04-24 07:36:19 +0800

    v1

---
+++ DivideTwoIntegers.cpp
@@ -0,0 +1,31 @@
+﻿class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        
+        long long a = dividend;
+        long long b = divisor;
+        
+        if(a < 0)
+            a = -a;
+        if(b < 0)
+            b = -b;
+        
+        long long result = 0;
+        
+        while(a >= b)
+        {
+            long long tmp = b;
+            
+            for(long long i = 1; a>=tmp; i<<=1, tmp<<=1)
+            {
+                a -=tmp;
+                result +=i;
+            }
+        }
+        
+        if(result >= INT_MAX && 0 == (dividend<0)^(divisor<0))
+            return INT_MAX;
+        
+        return (int)(dividend<0)^(divisor<0)? -result:result;
+    }
+};
