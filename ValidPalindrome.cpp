ec7615c1c2ba034dd4f3057be7bf5068dc929351
woods-jiang <woods.jiang.1@gmail.com>
2015-04-24 15:15:26 +0800

    v1

---
+++ ValidPalindrome.cpp
@@ -0,0 +1,39 @@
+﻿class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        if(s.size() <= 1)
+            return true;
+            
+        for(int i=0, j=s.size()-1; i<j; )
+        {
+            while(i < j && !alphanumeric(s[i]))
+                i++;
+            while(i < j && !alphanumeric(s[j]))
+                j--;
+                
+            if(i < j && toLower(s[i]) != toLower(s[j]))
+            {
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+private:
+    bool alphanumeric(char c)
+    {
+        if((c>='A'&& c<='Z') || (c>='a'&&c<='z') || (c>='0'&&c<='9') )
+            return true;
+        else
+            return false;
+    }
+    char toLower(char c)
+    {
+        if(c>='A' && c<='Z')
+            return c + 32;
+    }
+};
