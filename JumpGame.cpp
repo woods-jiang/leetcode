32371dc3d079f5e739a28250851906a148c5ce47
woods-jiang <woods.jiang.1@gmail.com>
2015-04-26 21:06:34 +0800

    v1

---
+++ JumpGame.cpp
@@ -0,0 +1,18 @@
+﻿class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int next = nums.size()-1;
+        
+        // 从倒数第二个元素开始，看它能否到达最后一个元素，一次类推
+        for(int i=next-1; i>=0; i--)
+        {
+            if(i+nums[i] >= next)
+            {
+                next = i;
+            }
+        }
+        
+        return next == 0;
+    }
+};
