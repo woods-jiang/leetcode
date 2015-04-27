66172c57ea07f60126615ade8c501f1556d44989
woods-jiang <woods.jiang.1@gmail.com>
2015-04-27 18:41:00 +0800

    v1

---
+++ ReverseWordsInAString.cpp
@@ -0,0 +1,54 @@
+﻿class Solution {
+public:
+    void reverseWords(string &s) {
+        
+        if( s.empty() )
+            return;
+
+        int i = 0;
+        int k = 0;
+
+        while(i<s.size() && s[i]==' ')
+        {
+            i++;
+        }
+
+        while(i < s.size() )
+        {
+            int start = k;
+            
+            while(i<s.size() && s[i]!=' ')
+            {
+                s[k++] = s[i++];
+            }
+                
+            if(k-start > 1)
+            {
+                reverseSingleWords(s, start, k-1);
+            }
+            
+            while(i<s.size() && s[i]==' ')
+            {
+                i++;
+            }
+            
+            if(i < s.size())
+            {
+                s[k++] = ' ';
+            }
+        }
+        
+        s = s.substr(0,k);
+        
+        reverseSingleWords(s, 0, s.size()-1);
+    }
+    
+private:
+    void reverseSingleWords(string &s, int k, int i)
+    {
+        for(; k<i; k++, i--)
+        {
+            swap(s[k], s[i]);
+        }
+    }
+};
