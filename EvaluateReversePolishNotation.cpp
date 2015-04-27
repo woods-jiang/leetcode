d99a055d851d23ff5ca14ae4250895428726270d
woods-jiang <woods.jiang.1@gmail.com>
2015-04-27 19:51:39 +0800

    v1

---
+++ EvaluateReversePolishNotation.cpp
@@ -0,0 +1,40 @@
+﻿class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+        stack<int> result;
+        
+        for(int i=0; i<tokens.size(); i++)
+        {
+            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/")
+            {
+                int num2 = result.top();
+                result.pop();
+                int num1 = result.top();
+                result.pop();
+                
+                if(tokens[i] == "+")
+                {
+                    result.push(num1+num2);
+                }
+                else if(tokens[i] == "-")
+                {
+                    result.push(num1-num2);
+                }
+                else if(tokens[i] == "*")
+                {
+                    result.push(num1*num2);
+                }
+                else
+                {
+                    result.push(num1/num2);
+                }
+            }
+            else
+            {
+                result.push(atoi(tokens[i].c_str()));
+            }
+        }
+        return result.top();
+    }
+};
