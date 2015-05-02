f549d79a49cbbd51940545cd3cb8dad42a10d16a
woods-jiang <woods.jiang.1@gmail.com>
2015-05-02 11:00:37 +0800

    v1

---
+++ IsomorphicStrings.cpp
@@ -0,0 +1,29 @@
+﻿class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+        if(s.size() != t.size())
+            return false;
+        
+        unordered_map<char,char> ms;
+        unordered_map<char,char> mt;
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            if(ms.find(s[i]) == ms.end())
+                ms[s[i]] = t[i];
+            else if (ms[s[i]] != t[i])
+                return false;
+        }
+        
+        for(int i=0; i<t.size(); i++)
+        {
+            if(mt.find(t[i]) == mt.end())
+                mt[t[i]] = s[i];
+            else if (mt[t[i]] != s[i])
+                return false;
+        }
+        
+        return true;
+    }
+};
