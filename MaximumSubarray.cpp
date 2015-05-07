4ec49e8d4c02b4657ac743d8d24a40323054cb0c
Woods Jiang <woods.jiang.1@gmail.com>
2015-05-07 21:20:15 +0800

    v1

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -5,6 +5,9 @@ public:
         int sum = 0;
         int result = INT_MIN;
         
+        // 状态方程 sum[i] = max(sum[i-1]+nums[i], nums[i]) 0<=i<n
+        // result = max(sum[i]) 0<=i<n
+        
         for(int i=0; i<nums.size(); i++)
         {
             sum = max(sum+nums[i], nums[i]);
