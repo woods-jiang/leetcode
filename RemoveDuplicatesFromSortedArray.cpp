cd10b8ea78dcc0a7fda0d8261de434c398bbd456
Woods Jiang <woods.jiang.1@gmail.com>
2015-05-12 14:45:24 +0800

    v2

--- RemoveDuplicatesFromSortedArray.cpp
+++ RemoveDuplicatesFromSortedArray.cpp
@@ -1,25 +1,31 @@
-﻿class Solution {
+class Solution {
 public:
-    int removeDuplicates(int A[], int n) {
+    int removeDuplicates(vector<int>& nums) {
         
-        if(0==n)
-            return 0;
-        if(1==n)
-            return 1;
-            
-        int j = 0;
-        
-        for(int i=1; i<n; i++)
+        if(nums.size() <= 1)
         {
-            if(A[i] != A[i-1])
-            {
-                j++;
-            }
-            
-            A[j] = A[i];
-            
+            return nums.size();
         }
         
-        return j+1;
+        vector<int>::iterator slow = nums.begin();
+        vector<int>::iterator it = nums.begin();
+        
+        for( ; (it+1) != nums.end(); ++it)
+        {
+            if(*it != *(it+1))
+            {
+                slow++;
+            }
+            *slow = *(it+1);
+        }
+        slow++;
+        
+        // 删除其他多余的元素
+        while(slow != nums.end())
+        {
+            nums.erase(slow);
+        }
+        
+        return nums.size();
     }
 };
