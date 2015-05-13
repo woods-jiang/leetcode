eeed9ed103a7a851f2e21a18b8a2a4613a062a9a
Woods Jiang <woods.jiang.1@gmail.com>
2015-05-13 08:04:36 +0800

    v2 头插法

--- ReverseLinkedList-II.cpp
+++ ReverseLinkedList-II.cpp
@@ -38,3 +38,49 @@ public:
         return head;
     }
 };
+
+// v2
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        
+        if(NULL == head || NULL == head->next || m >= n)
+        {
+            return head;
+        }
+        
+        ListNode dummy(-1);
+        dummy.next = head;
+        
+        ListNode *pCur = &dummy;
+        
+        for(int i=0; i<m-1; i++)
+        {
+            pCur = pCur->next;
+        }
+        
+        ListNode *head2 = pCur;
+        pCur = head2->next;
+        ListNode *pNext = pCur->next;
+        
+        // 使用头插法，每次将后面一个元素插入到head2前面
+        for(int i=m; i<n; i++)
+        {
+            pCur->next = pNext->next;
+            pNext->next = head2->next;
+            head2->next = pNext;
+            pNext = pCur->next;
+        }
+        
+        return dummy.next;
+    }
+};
